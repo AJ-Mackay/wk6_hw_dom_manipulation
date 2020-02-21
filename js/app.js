54d42acbfe7d5491c279da25865f51dc653bf087
Ruiner85 <paul.m.mackay85@gmail.com>
2020-02-21 17:11:25 +0000

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  const button = document.createElement('button');
+  const body - document.querySelector('body');
+  body.appendChild(button);
+  button.textContent = 'Clear List';
+  button.addEventListener('click', handleButtonClick);
+});
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  const newListItem = createWatchListItem(event.target);
+  const list = document.querySelector('ul');
+  list.appendChild(newListItem);
+  event.target.reset();
+};
+
+const createWatchListItem = function (form) {
+  const watchListItem = document.createElement('li');
+  watchListItem.classList.add('watch-list-item');
+
+  const title = document.createElement('h2');
+  title.textContent = form.title.value;
+  watchListItem.appendChild(title);
+
+  const year = document.createElement('h3');
+  year.textContent = form.year.value;
+  watchListItem.appendChild(year);
+
+  const rating = document.createElement('p');
+  rating.textContent = form.rating.value;
+  watchListItem.appendChild.(rating);
+
+  return watchListItem;
+}
+
+const handleButtonClick = function () {
+  const watchList = document.querySelector('ul');
+  watchList.innerHTML = '';
+};
